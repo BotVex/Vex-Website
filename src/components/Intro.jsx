6aa9bf8eef4fab5e92f9af8fc7d8fd1159eb1ecc
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-04-09 17:30:49 -0300

    redirect to invite page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,16 @@
 import "./css/Intro.css"
 
 const Intro = () => {
+    const redirectToInvitePage = () => {
+        window.location.href = " https://discord.com/oauth2/authorize?client_id=783716882896912405&permissions=1634200972535&scope=applications.commands%20bot"
+    }
+
     return (
         <div className="intro">
             <h1>Olá, eu sou a Vex!</h1>
             <p>Sou apenas um simples bot para o <span className="discord-color">Discord</span></p>
             <div className="buttons">
-                <button>Adicionar ao meu servidor</button>
+                <button onClick={redirectToInvitePage}>Adicionar ao meu servidor</button>
             </div>
         </div>
     )
