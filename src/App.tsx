68b45bbe4a3811aa2bc65ef183212e6e31a4b382
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-06-18 19:32:57 -0300

    use useNavigate and bugs fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
 // import Commands from "./pages/Commands"
-// import About from "./pages/About"
+import About from "./pages/About"
 import NotFound from "./pages/NotFound"
 
 
@@ -12,12 +12,16 @@ const App: React.FC = () => {
     <>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        {/* <Route path="commands" element={<Commands/>}/>
-        <Route path="about" element={<About/>}/> */}
+        {/* <Route path="commands" element={<Commands/>}/> */}
+        <Route path="/about" element={<About/>}/>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
+}
+
+export const redirect = (url: string, newTab: boolean = true) => {
+  window.open(url, (newTab) ? "_blank" : "")
 }
 
 export default App
